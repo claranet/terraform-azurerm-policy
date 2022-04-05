48b58eb0430e8763c00036f06385d3c4424e5a09
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-04-05 16:11:17 +0200

    AZ-515: Customize CAF resource

--- r-naming.tf
+++ r-naming.tf
@@ -1,8 +1,8 @@
 resource "azurecaf_name" "policy" {
-  name          = var.stack
+  name          = var.policy_display_name
   resource_type = "azurerm_resource_group"
   prefixes      = compact([local.name_prefix, var.use_caf_naming ? "policy" : ""])
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, var.use_caf_naming ? "" : "policy"])
+  suffixes      = compact([local.name_suffix, var.use_caf_naming ? "" : "policy"])
   use_slug      = false # to update when naming for policy is available
   clean_input   = true
   separator     = "-"
