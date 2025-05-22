bc820b68d10a0e6b6c38824428eb0c26a04b0216
Corentin Berthelot <corentin.berthelot@fr.clara.net>
2025-05-22 16:28:14 +0200

    feat(AZ-1562): output policy assignments identity principal ID

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,39 @@
 output "definition_id" {
-  value       = azurerm_policy_definition.main.id
   description = "Azure policy definition ID."
+  value       = azurerm_policy_definition.main.id
 }
 
 output "resource" {
-  value       = azurerm_policy_definition.main.id
   description = "Azure policy resource object."
+  value       = azurerm_policy_definition.main.id
+}
+
+output "policy_assignments_identity_principal_id" {
+  description = "The principal ID of the identity for policy assignments."
+  value = {
+    "management_group" = { for k, v in azurerm_management_group_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+    "subscription"     = { for k, v in azurerm_subscription_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+    "resource_group"   = { for k, v in azurerm_resource_group_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+    "resource"         = { for k, v in azurerm_resource_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+  }
+}
+
+output "policy_assignment_management_group_identity_principal_id" {
+  description = "The principal ID of the identity for management group policy assignments."
+  value       = { for k, v in azurerm_management_group_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+}
+
+output "policy_assignment_subscription_identity_principal_id" {
+  description = "The principal ID of the identity for subscription policy assignments."
+  value       = { for k, v in azurerm_subscription_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+}
+
+output "policy_assignment_resource_group_identity_principal_id" {
+  description = "The principal ID of the identity for resource group policy assignments."
+  value       = { for k, v in azurerm_resource_group_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
+}
+
+output "policy_assignment_resource_identity_principal_id" {
+  description = "The principal ID of the identity for resource-level policy assignments."
+  value       = { for k, v in azurerm_resource_policy_assignment.main : k => try(v.identity[0].principal_id, null) }
 }
